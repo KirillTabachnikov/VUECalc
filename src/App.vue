613421407d23c723ee0c5e0a216ace87b04e4680
KirillTabachnikov <tabachnikovkg@yandex.ru>
2022-09-02 20:37:19 +0300

    Calc

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <KalKuleted />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import KalKuleted from './components/KalKuleted.vue'
+
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    KalKuleted
   }
 }
 </script>
---
+++ src/components/KalKuleted.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <div class="main">
+            <input type="number" v-model.number="op1">
+            <input type="number" v-model.number="op2">
+            = {{ result }}
+        </div>
+        <div class="keybord">
+            <button @click="sum">+</button>
+            <button @click="div">/</button>
+            <button @click="sub">-</button>
+            <button @click="mult">*</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'KalKuleted',
+    data: () => ({
+        op1: 0,
+        op2: 0,
+        result: 0
+    }),
+    methods: {
+        sum() {
+            this.result = this.op1 + this.op2
+        },
+        div() {
+            this.result = this.op1 / this.op2
+        },
+        sub() {
+            this.result = this.op1 - this.op2
+        },
+        mult() {
+            this.result = this.op1 * this.op2
+        }
+    }
+
+}
+</script>
